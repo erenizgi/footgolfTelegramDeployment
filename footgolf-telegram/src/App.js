7bc6d4dd3803a6c3c5f195917d9e2926d660cc18
erenizgi <erenizgi001@gmail.com>
2024-05-30 17:50:08 +0300

    React router set up

--- footgolf-telegram/src/App.js
+++ footgolf-telegram/src/App.js
@@ -1,16 +1,17 @@
 import './App.css';
+import {Link, Route, BrowserRouter as Router, Routes} from "react-router-dom";
+import MainPage from "./pages/MainPage/MainPage";
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <div style={{textAlign: "center", alignItems: "center", paddingTop: 200}}>
-        <p>Merhaba arkadaşlar, burada çalışırken lütfen App.js, index.js ve package.json dosyasına dokunmayalım. &nbsp;</p>
-        Push atarken bu dosyaları dahil etmeyelim ve kendi branchimizde çalışalım. Size göndermiş olduğum tutorialdaki conventionlara göre hareket edersek hepimiz çok daha hızlı çalışırız. &nbsp;
-        <p>Yazacağınız componentlar için component klasörüne gerekli klasörü açın. Css dosyalarınızı da orada tutun.</p>
-        <p>Sayfaya sağ tıklayıp incele kısmından gereken ekran ölçülerini (Iphone 12) seçerseniz rahat edersiniz.</p>
-      </div>
-    </div>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<MainPage/>}></Route>
+            </Routes>
+        </Router>
+
+    );
 }
 
 export default App;
